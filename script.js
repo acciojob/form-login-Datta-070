e148af901f458dce4c486f59ff495ef0b69f63f3
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-10-22 12:11:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	alert(firstName.value+" "+secondName.value);
 }
+
+const firstName = document.getElementsByTagName("input")[0]
+const secondName= document.getElementsByTagName("input")[1]
